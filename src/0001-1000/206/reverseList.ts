97692b031bd20b4bc4878299eed5ba4da5b3f77a
dandelion-huang <dandelion820308@gmail.com>
2024-03-22 14:32:42 +0800

    fix: 🐛 fix an any type assertion

--- src/0001-1000/206/reverseList.ts
+++ src/0001-1000/206/reverseList.ts
@@ -5,7 +5,7 @@ import { ListNode } from 'utils/SinglyLinkedListNode'
 
 function reverseList(head: ListNode | null): ListNode | null {
     // 1. define a prev to store the previous node
-    let prev = null
+    let prev: ListNode | null = null
 
     // 2. iterate through the linked list
     while (head !== null) {

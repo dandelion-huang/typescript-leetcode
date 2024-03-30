099245b22905254e41563e3cfb38df2e64b1e759
dandelion-huang <dandelion820308@gmail.com>
2024-03-30 22:11:35 +0800

    docs: ✏️ add the steps of the solution

--- src/0001-1000/687/longestUnivaluePath.ts
+++ src/0001-1000/687/longestUnivaluePath.ts
@@ -6,6 +6,7 @@ import { TreeNode } from 'utils/BinaryTreeNode'
 function longestUnivaluePath(root: TreeNode | null): number {
     let ans = 0
 
+    // 1. dfs
     function dfs(node: TreeNode | null, prevVal?: number): number {
         if (node === null) {
             return 0
@@ -14,12 +15,15 @@ function longestUnivaluePath(root: TreeNode | null): number {
         const leftLength = dfs(node.left, node.val)
         const rightLength = dfs(node.right, node.val)
 
+        // 2. update the answer
         ans = Math.max(ans, leftLength + rightLength)
 
+        // 3. if the current node is not the same as the previous node, length is 0
         if (node.val !== prevVal) {
             return 0
         }
 
+        // 4. return the length of the path
         return 1 + Math.max(leftLength, rightLength)
     }
 

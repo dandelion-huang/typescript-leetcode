9a59a340d3f20f76a80e2cbb84e26f38ca072ca8
dandelion-huang <dandelion820308@gmail.com>
2024-04-03 17:35:43 +0800

    feat: 🎸 leetcode 144

---
+++ src/0001-1000/144/preorderTraversal.ts
@@ -0,0 +1,26 @@
+import { TreeNode } from 'utils/BinaryTreeNode'
+
+// Time: O(n)
+// Space: O(n)
+
+function preorderTraversal(root: TreeNode | null): number[] {
+    const ans: number[] = []
+
+    // 1. dfs
+    function dfs(node: TreeNode | null): void {
+        if (node === null) {
+            return
+        }
+
+        // 2. preorder traversal
+        ans.push(node.val)
+        dfs(node.left)
+        dfs(node.right)
+    }
+
+    dfs(root)
+
+    return ans
+}
+
+export { preorderTraversal }

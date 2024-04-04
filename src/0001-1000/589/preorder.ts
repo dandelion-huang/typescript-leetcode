5641ea7aa5df459df1939c4048b83abbe4f1242f
dandelion-huang <dandelion820308@gmail.com>
2024-04-04 17:08:44 +0800

    feat: 🎸 leetcode 589

---
+++ src/0001-1000/589/preorder.ts
@@ -0,0 +1,27 @@
+import { Node } from 'utils/N-aryTreeNode'
+
+// Time: O(n)
+// Space: O(n)
+
+function preorder(root: Node | null): number[] {
+    const ans: number[] = []
+
+    // 1. dfs
+    function dfs(node: Node | null): void {
+        if (node === null) {
+            return
+        }
+
+        // 2. preorder traversal
+        ans.push(node.val)
+        for (const child of node.children) {
+            dfs(child)
+        }
+    }
+
+    dfs(root)
+
+    return ans
+}
+
+export { preorder }

96c90c69f9dec81ce0df4c74314a936eb8b10b79
dandelion-huang <dandelion820308@gmail.com>
2024-04-07 23:13:29 +0800

    feat: 🎸 new solution for leetcode 102

---
+++ src/0001-1000/102/levelOrder3.ts
@@ -0,0 +1,45 @@
+import { TreeNode } from 'utils/BinaryTreeNode'
+
+// <Queue>
+// Time: O(n)
+// Space: O(n)
+
+function levelOrder(root: TreeNode | null): number[][] {
+    const ans: number[][] = []
+
+    // 1. bfs
+    function bfs(node: TreeNode | null): void {
+        // edge cases
+        if (node === null) {
+            return
+        }
+
+        // 2. level-order traversal
+        const queue: TreeNode[] = [node]
+        let curLevelSize: number
+
+        while (queue.length) {
+            curLevelSize = queue.length
+            ans.push([])
+
+            for (let i = 0; i < curLevelSize; ++i) {
+                const curNode = queue.shift()!
+                ans[ans.length - 1].push(curNode.val)
+
+                if (curNode.left) {
+                    queue.push(curNode.left)
+                }
+
+                if (curNode.right) {
+                    queue.push(curNode.right)
+                }
+            }
+        }
+    }
+
+    bfs(root)
+
+    return ans
+}
+
+export { levelOrder }

5d12cd313cf5e96b58b4a0aed92e0d7f5f63b555
dandelion-huang <dandelion820308@gmail.com>
2024-04-08 23:51:37 +0800

    feat: 🎸 new solutions for leetcode 234

---
+++ src/0001-1000/234/isPalindrome2.ts
@@ -0,0 +1,31 @@
+import { ListNode } from 'utils/SinglyLinkedListNode'
+
+// <Two Pointers>
+// Time: O(n)
+// Space: O(n)
+
+function isPalindrome(head: ListNode | null): boolean {
+    // edge cases
+    if (!head?.next) {
+        return true
+    }
+
+    // 1. copy every nodes in an array
+    const values: number[] = []
+
+    while (head) {
+        values.push(head.val)
+        head = head.next
+    }
+
+    // 2. two pointers
+    for (let i = 0, j = values.length - 1; i < j; ++i, --j) {
+        if (values[i] !== values[j]) {
+            return false
+        }
+    }
+
+    return true
+}
+
+export { isPalindrome }
---
+++ src/0001-1000/234/isPalindrome3.ts
@@ -0,0 +1,35 @@
+import { ListNode } from 'utils/SinglyLinkedListNode'
+
+// <Recursion>
+// Time: O(n)
+// Space: O(n)
+
+function isPalindrome(head: ListNode | null): boolean {
+    // edge cases
+    if (!head?.next) {
+        return true
+    }
+
+    let front: ListNode | null = head
+
+    //
+    function recursion(node: ListNode | null): boolean {
+        if (node) {
+            if (!recursion(node.next)) {
+                return false
+            }
+
+            if (front!.val !== node.val) {
+                return false
+            }
+
+            front = front!.next
+        }
+
+        return true
+    }
+
+    return recursion(head)
+}
+
+export { isPalindrome }

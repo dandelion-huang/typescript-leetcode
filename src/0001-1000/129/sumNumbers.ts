bd7278c84fc2c41e7d952771756cf40ef12f5b62
dandelion-huang <dandelion820308@gmail.com>
2024-04-16 15:06:36 +0800

    docs: ✏️ update the steps of the solution for leetcode 129

--- src/0001-1000/129/sumNumbers.ts
+++ src/0001-1000/129/sumNumbers.ts
@@ -5,12 +5,13 @@ import { TreeNode } from 'utils/BinaryTreeNode'
 // Space: O(n)
 
 function sumNumbers(root: TreeNode | null): number {
-    // dfs
+    // 1. dfs
     function dfs(node: TreeNode | null, prevSum: number = 0): number {
         if (node === null) {
             return 0
         }
 
+        // 2. pre-order traversal
         const sum = prevSum * 10 + node.val
 
         if (node.left === null && node.right === null) {

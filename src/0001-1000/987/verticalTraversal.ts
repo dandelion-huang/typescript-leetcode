887c1d27fe9f66559a2d4216a06cc0f3b588143f
dandelion-huang <dandelion820308@gmail.com>
2024-04-16 15:46:46 +0800

    feat: 🎸 leetcode 987

---
+++ src/0001-1000/987/verticalTraversal.ts
@@ -0,0 +1,57 @@
+import { TreeNode } from 'utils/BinaryTreeNode'
+
+// <DFS>
+// Time: O(nlogn) (Sorting)
+// Space: O(n)
+
+// node: [col, row, val]
+type NodeTuple = [number, number, number]
+
+// 1. dfs
+function dfs(node: TreeNode | null, col: number, row: number, nodes: NodeTuple[]): void {
+    // edge cases
+    if (node === null) {
+        return
+    }
+
+    // 2. pre-order traversal
+    nodes.push([col, row, node.val])
+    dfs(node.left, col - 1, row + 1, nodes)
+    dfs(node.right, col + 1, row + 1, nodes)
+}
+
+function verticalTraversal(root: TreeNode | null): number[][] {
+    const nodes: NodeTuple[] = []
+
+    dfs(root, 0, 0, nodes)
+
+    // 3. sorting with the col, row, and val
+    nodes.sort(([colA, rowA, valA], [colB, rowB, valB]) => {
+        if (colA !== colB) {
+            return colA - colB
+        }
+
+        if (rowA !== rowB) {
+            return rowA - rowB
+        }
+
+        return valA - valB
+    })
+
+    // 4. traverse the nodes
+    const ans: number[][] = []
+    let lastCol = -Infinity
+
+    for (const [col, , val] of nodes) {
+        if (col !== lastCol) {
+            ans.push([])
+            lastCol = col
+        }
+
+        ans[ans.length - 1].push(val)
+    }
+
+    return ans
+}
+
+export { verticalTraversal }

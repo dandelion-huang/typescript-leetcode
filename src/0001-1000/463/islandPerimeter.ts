22211016da38b8e8d0db282c5f6284ed6abafb82
dandelion-huang <dandelion820308@gmail.com>
2024-04-19 00:39:47 +0800

    refactor: 💡 reduce the cognitive complexity
    Refactor this function to reduce its Cognitive Complexity from 16 to the
    15 allowed.

--- src/0001-1000/463/islandPerimeter.ts
+++ src/0001-1000/463/islandPerimeter.ts
@@ -8,23 +8,28 @@ function islandPerimeter(grid: number[][]): number {
     const [n, m] = [grid.length, grid[0].length]
     let ans = 0
 
+    // calculate the sides of the island
+    function getSides(x: number, y: number): number {
+        let sides = 0
+
+        for (let k = 0; k < 4; ++k) {
+            const [px, py] = [x + dx[k], y + dy[k]]
+
+            // if the current cell touches the edge of the grid or water
+            if (px < 0 || px >= n || py < 0 || py >= m || !grid[px][py]) {
+                ++sides
+            }
+        }
+
+        return sides
+    }
+
     // 1. traverse the grid
     for (let i = 0; i < n; ++i) {
         for (let j = 0; j < m; ++j) {
             // 2. examine if the current cell is an island
             if (grid[i][j]) {
-                let sides = 0
-
-                for (let k = 0; k < 4; ++k) {
-                    const [px, py] = [i + dx[k], j + dy[k]]
-
-                    // if the current cell touches the edge of the grid or water
-                    if (px < 0 || px >= n || py < 0 || py >= m || !grid[px][py]) {
-                        ++sides
-                    }
-                }
-
-                ans += sides
+                ans += getSides(i, j)
             }
         }
     }

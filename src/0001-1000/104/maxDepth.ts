cd971f529329b4f4c63196143fbf6383d89a0137
dandelion-huang <dandelion820308@gmail.com>
2024-04-19 11:43:29 +0800

    feat: 🎸 leetcode 104

---
+++ src/0001-1000/104/maxDepth.ts
@@ -0,0 +1,17 @@
+import { TreeNode } from 'utils/BinaryTreeNode'
+
+// <Recursion, DFS>
+// Time: O(n)
+// Space: O(n)
+
+function maxDepth(root: TreeNode | null): number {
+    // edge cases
+    if (!root) {
+        return 0
+    }
+
+    // 1. dfs
+    return Math.max(maxDepth(root.left), maxDepth(root.right)) + 1
+}
+
+export { maxDepth }
---
+++ src/0001-1000/104/maxDepth2.ts
@@ -0,0 +1,38 @@
+import { TreeNode } from 'utils/BinaryTreeNode'
+
+// <Queue, BFS>
+// Time: O(n)
+// Space: O(n)
+
+function maxDepth(root: TreeNode | null): number {
+    // edge cases
+    if (!root) {
+        return 0
+    }
+
+    const queue: (TreeNode | null)[] = [root]
+    let ans = 0
+
+    // 1. bfs
+    while (queue.length) {
+        const size = queue.length
+
+        for (let i = 0; i < size; ++i) {
+            const curNode = queue.shift()!
+
+            if (curNode.left) {
+                queue.push(curNode.left)
+            }
+
+            if (curNode.right) {
+                queue.push(curNode.right)
+            }
+        }
+
+        ++ans
+    }
+
+    return ans
+}
+
+export { maxDepth }

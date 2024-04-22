f3ae7b24adec38c2644fcf2b435be36d14aeffeb
dandelion-huang <dandelion820308@gmail.com>
2024-04-22 11:48:32 +0800

    feat: 🎸 leetcode 814

---
+++ src/0001-1000/814/pruneTree.ts
@@ -0,0 +1,24 @@
+import { TreeNode } from 'utils/BinaryTreeNode'
+
+// <Recursion, DFS>
+// Time: O(n)
+// Space: O(n)
+
+function pruneTree(root: TreeNode | null): TreeNode | null {
+    // edge cases
+    if (!root) {
+        return null
+    }
+
+    root.left = pruneTree(root.left)
+    root.right = pruneTree(root.right)
+
+    // 2. post-order traversal
+    if (!root.left && !root.right && root.val === 0) {
+        return null
+    }
+
+    return root
+}
+
+export { pruneTree }

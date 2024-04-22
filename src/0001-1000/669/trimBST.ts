3c5b5edf1db3e1ccad5c21de5888e63746fb987a
dandelion-huang <dandelion820308@gmail.com>
2024-04-22 13:25:38 +0800

    feat: 🎸 leetcode 669

---
+++ src/0001-1000/669/trimBST.ts
@@ -0,0 +1,27 @@
+import { TreeNode } from 'utils/BinaryTreeNode'
+
+// <Recursion, DFS>
+// Time: O(n)
+// Space: O(n)
+
+function trimBST(root: TreeNode | null, low: number, high: number): TreeNode | null {
+    // edge cases
+    if (!root) {
+        return null
+    }
+
+    if (root.val < low) {
+        return trimBST(root.right, low, high)
+    }
+
+    if (root.val > high) {
+        return trimBST(root.left, low, high)
+    }
+
+    root.left = trimBST(root.left, low, high)
+    root.right = trimBST(root.right, low, high)
+
+    return root
+}
+
+export { trimBST }
---
+++ src/0001-1000/669/trimBST2.ts
@@ -0,0 +1,46 @@
+import { TreeNode } from 'utils/BinaryTreeNode'
+
+// <Iteration, DFS>
+// Time: O(n)
+// Space: O(1)
+
+function trimBST(root: TreeNode | null, low: number, high: number): TreeNode | null {
+    // handle the root
+    while (root && (root.val < low || root.val > high)) {
+        if (root.val < low) {
+            root = root.right
+        } else {
+            root = root.left
+        }
+    }
+
+    // edge cases
+    if (!root) {
+        return null
+    }
+
+    let node: TreeNode = root
+
+    // 1. dfs
+    while (node.left) {
+        if (node.left.val < low) {
+            node.left = node.left.right
+        } else {
+            node = node.left
+        }
+    }
+
+    node = root
+
+    while (node.right) {
+        if (node.right.val > high) {
+            node.right = node.right.left
+        } else {
+            node = node.right
+        }
+    }
+
+    return root
+}
+
+export { trimBST }

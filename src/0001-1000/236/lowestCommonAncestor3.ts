4f9e683a85f72828ed54c8f0758803f4189f2c01
dandelion-huang <dandelion820308@gmail.com>
2024-04-26 15:07:24 +0800

    feat: 🎸 new solution for leetcode 236

---
+++ src/0001-1000/236/lowestCommonAncestor3.ts
@@ -0,0 +1,32 @@
+import { TreeNode } from 'utils/BinaryTreeNode'
+
+// <Recursion, DFS>
+// Time: O(n)
+// Space: O(n)
+
+function lowestCommonAncestor(
+    root: TreeNode | null,
+    p: TreeNode | null,
+    q: TreeNode | null
+): TreeNode | null {
+    // edge cases
+    if (root === null || root === p || root === q) {
+        return root
+    }
+
+    const leftChild = lowestCommonAncestor(root.left, p, q)
+    const rightChild = lowestCommonAncestor(root.right, p, q)
+
+    // constraints: p and q will exist in the BST
+    if (!leftChild) {
+        return rightChild
+    }
+
+    if (!rightChild) {
+        return leftChild
+    }
+
+    return root
+}
+
+export { lowestCommonAncestor }

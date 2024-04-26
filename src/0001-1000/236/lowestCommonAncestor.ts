797ed471187a147282d54a8c854f40dedd313710
dandelion-huang <dandelion820308@gmail.com>
2024-04-26 12:07:58 +0800

    feat: 🎸 leetcode 236

---
+++ src/0001-1000/236/lowestCommonAncestor.ts
@@ -0,0 +1,38 @@
+import { TreeNode } from 'utils/BinaryTreeNode'
+
+// <Recursion, DFS>
+// Time: O(n)
+// Space: O(n)
+
+function lowestCommonAncestor(
+    root: TreeNode | null,
+    p: TreeNode | null,
+    q: TreeNode | null
+): TreeNode | null {
+    let ans: TreeNode | null = null
+
+    // 1. dfs
+    function dfs(node: TreeNode | null): boolean {
+        // edge cases
+        if (!node) {
+            return false
+        }
+
+        const leftChild = dfs(node.left)
+        const rightChild = dfs(node.right)
+        const isAncestor = node.val === p?.val || node.val === q?.val
+
+        // 2. post-order traversal
+        if ((leftChild && rightChild) || ((leftChild || rightChild) && isAncestor)) {
+            ans = node
+        }
+
+        return leftChild || rightChild || isAncestor
+    }
+
+    dfs(root)
+
+    return ans
+}
+
+export { lowestCommonAncestor }
---
+++ src/0001-1000/236/lowestCommonAncestor2.ts
@@ -0,0 +1,53 @@
+import { TreeNode } from 'utils/BinaryTreeNode'
+
+// <Recursion, DFS, HashTable>
+// Time: O(n)
+// Space: O(n)
+
+function lowestCommonAncestor(
+    root: TreeNode | null,
+    p: TreeNode | null,
+    q: TreeNode | null
+): TreeNode | null {
+    // constraints: all Node.val are unique
+    const parent = new Map<number, TreeNode>()
+    const visited = new Map<number, boolean>()
+
+    // 1. dfs
+    function dfs(node: TreeNode | null): void {
+        // edge cases
+        if (!node) {
+            return
+        }
+
+        if (node.left) {
+            parent.set(node.left.val, node)
+            dfs(node.left)
+        }
+
+        if (node.right) {
+            parent.set(node.right.val, node)
+            dfs(node.right)
+        }
+    }
+
+    dfs(root)
+
+    // find the lowest common ancestor
+    while (p) {
+        visited.set(p.val, true)
+        p = parent.get(p.val) ?? null
+    }
+
+    while (q) {
+        if (visited.get(q.val)) {
+            return q
+        }
+
+        q = parent.get(q.val) ?? null
+    }
+
+    return null
+}
+
+export { lowestCommonAncestor }

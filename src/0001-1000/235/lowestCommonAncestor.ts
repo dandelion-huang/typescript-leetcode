3498bc76dd077536158a70b28e32e47169ddc264
dandelion-huang <dandelion820308@gmail.com>
2024-04-26 15:07:44 +0800

    feat: 🎸 leetcode 235

---
+++ src/0001-1000/235/lowestCommonAncestor.ts
@@ -0,0 +1,31 @@
+import { TreeNode } from 'utils/BinaryTreeNode'
+
+// Notice that the root is the root of the BST
+// So we can optimize the solution on the basis of leetcode 236
+
+// <Recursion, DFS>
+// Time: O(n)
+// Space: O(1)
+
+function lowestCommonAncestor(
+    root: TreeNode | null,
+    p: TreeNode | null,
+    q: TreeNode | null
+): TreeNode | null {
+    // edge cases
+    if (root === null || root === p || root === q) {
+        return root
+    }
+
+    if (p!.val < root.val && q!.val < root.val) {
+        return lowestCommonAncestor(root.left, p, q)
+    }
+
+    if (p!.val > root.val && q!.val > root.val) {
+        return lowestCommonAncestor(root.right, p, q)
+    }
+
+    return root
+}
+
+export { lowestCommonAncestor }
---
+++ src/0001-1000/235/lowestCommonAncestor2.ts
@@ -0,0 +1,51 @@
+import { TreeNode } from 'utils/BinaryTreeNode'
+
+// <Iteration, DFS>
+// Time: O(n)
+// Space: O(n)
+
+function getPath(node: TreeNode, target: TreeNode): TreeNode[] {
+    const path: TreeNode[] = []
+
+    while (node !== target) {
+        path.push(node)
+
+        if (target.val < node.val) {
+            node = node.left!
+        } else {
+            node = node.right!
+        }
+    }
+
+    path.push(target)
+
+    return path
+}
+
+function lowestCommonAncestor(
+    root: TreeNode | null,
+    p: TreeNode | null,
+    q: TreeNode | null
+): TreeNode | null {
+    // edge cases
+    if (!root) {
+        return null
+    }
+
+    const pathP = getPath(root, p!)
+    const pathQ = getPath(root, q!)
+
+    // find the lowest common ancestor
+    const minLength = Math.min(pathP.length, pathQ.length)
+    let ancestor: TreeNode | null = null
+
+    for (let i = 0; i < minLength; ++i) {
+        if (pathP[i] === pathQ[i]) {
+            ancestor = pathP[i]
+        }
+    }
+
+    return ancestor
+}
+
+export { lowestCommonAncestor }

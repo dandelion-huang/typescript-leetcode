744bea29f1c2e48957f4a800bd38ad9084c6dc9c
dandelion-huang <dandelion820308@gmail.com>
2024-05-01 15:48:19 +0800

    feat: 🎸 leetcode 901

---
+++ src/0001-1000/901/StockSpanner.ts
@@ -0,0 +1,34 @@
+// <Monotonic Stack>
+// Time: O(n)
+// Space: O(n)
+
+class StockSpanner {
+    stack: number[][]
+    index: number
+    constructor() {
+        this.stack = [[-1, Infinity]] // [index, price]
+        this.index = -1 // for day: -1
+    }
+
+    next(price: number): number {
+        ++this.index
+
+        while (this.stack[this.stack.length - 1][1] <= price) {
+            this.stack.pop()
+        }
+
+        const ans = this.index - this.stack[this.stack.length - 1][0]
+
+        this.stack.push([this.index, price])
+
+        return ans
+    }
+}
+
+/**
+ * Your StockSpanner object will be instantiated and called as such:
+ * var obj = new StockSpanner()
+ * var param_1 = obj.next(price)
+ */
+
+export { StockSpanner }

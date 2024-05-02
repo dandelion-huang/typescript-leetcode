84b1510c02b0fff178c39834ac6b1db8542c6092
dandelion-huang <dandelion820308@gmail.com>
2024-05-02 16:31:27 +0800

    feat: 🎸 leetcode 1019

---
+++ src/1001-2000/1019/nextLargerNodes.ts
@@ -0,0 +1,29 @@
+import { ListNode } from 'utils/SinglyLinkedListNode'
+
+function nextLargerNodes(head: ListNode | null): number[] {
+    // edge cases
+    if (!head) {
+        return []
+    }
+
+    const ans: number[] = []
+    const stack: number[][] = []
+    let index = -1
+    let cur: ListNode | null = head
+
+    while (cur) {
+        ++index
+        ans.push(0)
+
+        while (stack.length && cur.val > stack[stack.length - 1][1]) {
+            ans[stack.pop()![0]] = cur.val
+        }
+
+        stack.push([index, cur.val])
+        cur = cur.next
+    }
+
+    return ans
+}
+
+export { nextLargerNodes }

7a91b9aa59d8a383b0ecb5bcb80c8cd569de19cd
dandelion-huang <dandelion820308@gmail.com>
2024-05-02 23:00:34 +0800

    feat: 🎸 leetcode 3127

---
+++ src/3001-4000/3127/canMakeSquare.ts
@@ -0,0 +1,26 @@
+// <Brute Force>
+// Time: O(n^2 * k^2)
+// Space: O(1)
+
+// (x, y) is the top-left corner of the square
+function hasImplicitSquare(grid: string[][], x: number, y: number): boolean {
+    let bCount = 0
+
+    if (grid[x][y] === 'B') ++bCount
+    if (grid[x + 1][y] === 'B') ++bCount
+    if (grid[x][y + 1] === 'B') ++bCount
+    if (grid[x + 1][y + 1] === 'B') ++bCount
+
+    return bCount !== 2
+}
+
+function canMakeSquare(grid: string[][]): boolean {
+    return (
+        hasImplicitSquare(grid, 0, 0) ||
+        hasImplicitSquare(grid, 1, 0) ||
+        hasImplicitSquare(grid, 0, 1) ||
+        hasImplicitSquare(grid, 1, 1)
+    )
+}
+
+export { canMakeSquare }
---
+++ src/3001-4000/3127/canMakeSquare2.ts
@@ -0,0 +1,44 @@
+// <Prefix Sum>
+// Time: O(n^2)
+// Space: O(k^2)
+
+function prefixSum(grid: string[][]) {
+    const n = grid.length
+    const preSum = new Array(n + 1).fill(0).map(() => new Array(n + 1).fill(0))
+
+    for (let i = 0; i < n; ++i) {
+        for (let j = 0; j < n; ++j) {
+            preSum[i + 1][j + 1] =
+                preSum[i + 1][j] + preSum[i][j + 1] - preSum[i][j] + (grid[i][j] === 'B' ? 1 : 0)
+        }
+    }
+
+    return preSum
+}
+
+// (x, y) is the bottom-right corner of the square
+function hasImplicitSquare(preSum: number[][], x: number, y: number, k: number): boolean {
+    const bCount =
+        preSum[x + 1][y + 1] -
+        preSum[x - k + 1][y + 1] -
+        preSum[x + 1][y - k + 1] +
+        preSum[x - k + 1][y - k + 1]
+
+    return bCount <= 1 || bCount >= k ** 2 - 1
+}
+
+function canMakeSquare(grid: string[][]): boolean {
+    const n = grid.length
+    const k = n - 1
+    const preSum = prefixSum(grid)
+
+    for (let i = k - 1; i < n; ++i) {
+        for (let j = k - 1; j < n; ++j) {
+            if (hasImplicitSquare(preSum, i, j, k)) return true
+        }
+    }
+
+    return false
+}
+
+export { canMakeSquare }

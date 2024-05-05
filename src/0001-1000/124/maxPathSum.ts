24e857331ec66430e587017f5abd16c3b047f1fd
dandelion-huang <dandelion820308@gmail.com>
2024-05-05 14:08:15 +0800

    feat: 🎸 leetcode 124

---
+++ src/0001-1000/124/maxPathSum.ts
@@ -0,0 +1,31 @@
+import { TreeNode } from 'utils/BinaryTreeNode'
+
+// <Recursion, DFS>
+// Time: O(n)
+// Space: O(n)
+
+// 1. dfs
+function dfs(node: TreeNode | null, maxSum: { value: number }): number {
+    if (!node) {
+        return 0
+    }
+
+    // if negative, treat it as 0
+    const leftSum = Math.max(dfs(node.left, maxSum), 0)
+    const rightSum = Math.max(dfs(node.right, maxSum), 0)
+    const sum = node.val + leftSum + rightSum
+
+    maxSum.value = Math.max(maxSum.value, sum)
+
+    return node.val + Math.max(leftSum, rightSum)
+}
+
+function maxPathSum(root: TreeNode | null): number {
+    const maxSum = { value: -Infinity }
+
+    dfs(root, maxSum)
+
+    return maxSum.value
+}
+
+export { maxPathSum }

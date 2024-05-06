daf30b6d24568f97a290ccc3543c5406042b4cd5
dandelion-huang <dandelion820308@gmail.com>
2024-05-06 16:39:20 +0800

    feat: 🎸 revisit leetcode 687

--- src/0001-1000/687/longestUnivaluePath.ts
+++ src/0001-1000/687/longestUnivaluePath.ts
@@ -6,33 +6,30 @@ import { TreeNode } from 'utils/BinaryTreeNode'
 // Time: O(n)
 // Space: O(n)
 
-function longestUnivaluePath(root: TreeNode | null): number {
-    let ans = 0
-
-    // 1. dfs
-    function dfs(node: TreeNode | null, prevVal?: number): number {
-        if (!node) {
-            return 0
-        }
-
-        const leftLength = dfs(node.left, node.val)
-        const rightLength = dfs(node.right, node.val)
-
-        // 2. update the answer
-        ans = Math.max(ans, leftLength + rightLength)
-
-        // 3. if the current node is not the same as the previous node, length is 0
-        if (node.val !== prevVal) {
-            return 0
-        }
-
-        // 4. return the length of the path
-        return 1 + Math.max(leftLength, rightLength)
+// 1. dfs
+function dfs(node: TreeNode | null, longestPath: { value: number }, prevVal?: number): number {
+    if (!node) {
+        return 0
     }
 
-    dfs(root)
+    const leftLength = dfs(node.left, longestPath, node.val)
+    const rightLength = dfs(node.right, longestPath, node.val)
 
-    return ans
+    longestPath.value = Math.max(longestPath.value, leftLength + rightLength)
+
+    if (node.val !== prevVal) {
+        return 0
+    }
+
+    return Math.max(leftLength, rightLength) + 1
+}
+
+function longestUnivaluePath(root: TreeNode | null): number {
+    const longestPath = { value: 0 }
+
+    dfs(root, longestPath)
+
+    return longestPath.value
 }
 
 export { longestUnivaluePath }

2705d58f60f416dfc7c497aab2b4e93696b0cf33
dandelion-huang <dandelion820308@gmail.com>
2024-05-10 12:11:31 +0800

    feat: 🎸 leetcode 153

---
+++ src/0001-1000/153/findMin2.ts
@@ -0,0 +1,31 @@
+// <Binary Search, DivideAndConquer>
+// Time: O(nlogn)
+// Space: O(logn)
+
+// 1. divide and conquer
+function findMinDnC(nums: number[], l: number, r: number): number {
+    if (l === r) {
+        return nums[l]
+    }
+
+    const mid = (l + r) >>> 1
+    const left = findMinDnC(nums, l, mid)
+    const right = findMinDnC(nums, mid + 1, r)
+
+    if (left === right) {
+        return left
+    }
+
+    return Math.min(left, right)
+}
+
+function findMin(nums: number[]): number {
+    // edge cases
+    if (!nums.length) {
+        return NaN
+    }
+
+    return findMinDnC(nums, 0, nums.length - 1)
+}
+
+export { findMin }

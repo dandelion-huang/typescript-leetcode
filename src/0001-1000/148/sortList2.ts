6a0b8882a29d4a7adb3b86ea97688992fe475f2c
dandelion-huang <dandelion820308@gmail.com>
2024-06-04 16:42:30 +0800

    refactor: 💡 enhance the simplicity of the function
    Refactor the sortList function to reduce its cognitive complexity.
    However, this may impact the performance.

--- src/0001-1000/148/sortList2.ts
+++ src/0001-1000/148/sortList2.ts
@@ -5,6 +5,15 @@ import { mergeTwoLists } from '0001-1000/21/mergeTwoLists2'
 // Time: O(nlogn)
 // Space: O(1)
 
+// move pointer in length-segmented linked-list or to the tail
+function movePointer(node: ListNode | null, segmentLength: number): ListNode | null {
+    for (let i = 1; i < segmentLength && node?.next; ++i) {
+        node = node.next
+    }
+
+    return node
+}
+
 function sortList(head: ListNode | null): ListNode | null {
     // edge cases
     if (!head?.next) {
@@ -28,18 +37,13 @@ function sortList(head: ListNode | null): ListNode | null {
         while (cur) {
             const head1 = cur
 
-            for (let i = 1; i < subLength && cur.next; ++i) {
-                cur = cur.next
-            }
+            cur = movePointer(cur, subLength)
 
-            const head2: ListNode | null = cur.next
+            const head2: ListNode | null = cur!.next
 
-            cur.next = null // break the linked-list
+            cur!.next = null // break the linked-list
             cur = head2
-
-            for (let i = 1; i < subLength && cur?.next; ++i) {
-                cur = cur?.next
-            }
+            cur = movePointer(cur, subLength)
 
             let next: ListNode | null = null
 
@@ -51,11 +55,7 @@ function sortList(head: ListNode | null): ListNode | null {
             const mergedHead = mergeTwoLists(head1, head2)
 
             prev!.next = mergedHead
-
-            while (prev!.next) {
-                prev = prev!.next
-            }
-
+            prev = movePointer(prev, Infinity) // move pointer to the tail
             cur = next
         }
     }

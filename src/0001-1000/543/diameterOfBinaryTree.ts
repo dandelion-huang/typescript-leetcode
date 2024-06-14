e84f1bb76533894819ea4bd02f7b6034a95553d1
dandelion-huang <dandelion820308@gmail.com>
2024-06-14 12:05:39 +0800

    refactor: 💡 revise type signature of function dfs

--- src/0001-1000/543/diameterOfBinaryTree.ts
+++ src/0001-1000/543/diameterOfBinaryTree.ts
@@ -5,13 +5,13 @@ import { TreeNode } from 'classes/BinaryTreeNode'
 // Space: O(n)
 
 // 1. dfs
-function dfs(root: TreeNode | null, maxDiameter: { value: number }): number {
-    if (!root) {
+function dfs(node: TreeNode | null, maxDiameter: { value: number }): number {
+    if (!node) {
         return 0
     }
 
-    const leftLength = dfs(root.left, maxDiameter)
-    const rightLength = dfs(root.right, maxDiameter)
+    const leftLength = dfs(node.left, maxDiameter)
+    const rightLength = dfs(node.right, maxDiameter)
 
     maxDiameter.value = Math.max(maxDiameter.value, leftLength + rightLength)
 

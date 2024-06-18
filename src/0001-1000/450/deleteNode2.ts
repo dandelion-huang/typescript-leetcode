1400578370fda59151c9db2558bb1eba8a3ccd58
dandelion-huang <dandelion820308@gmail.com>
2024-06-18 16:52:40 +0800

    refactor: 💡 reduce the cognitive complexity
    Refactor this function to reduce its Cognitive Complexity from 19 to the
    15 allowed.

--- src/0001-1000/450/deleteNode2.ts
+++ src/0001-1000/450/deleteNode2.ts
@@ -4,6 +4,39 @@ import { TreeNode } from 'classes/BinaryTreeNode'
 // Time: O(n)
 // Space: O(1)
 
+function updateCurHelper(cur: TreeNode) {
+    if (!cur.left && !cur.right) {
+        return null
+    }
+
+    if (!cur.left) {
+        return cur.right
+    }
+
+    if (!cur.right) {
+        return cur.left
+    }
+
+    let successor = cur.right
+    let sucParent = cur
+
+    while (successor.left) {
+        sucParent = successor
+        successor = successor.left // find the minimum node in the right subtree
+    }
+
+    if (sucParent.val === cur.val) {
+        sucParent.right = successor.right
+    } else {
+        sucParent.left = successor.right
+    }
+
+    successor.left = cur.left
+    successor.right = cur.right
+
+    return successor // replace the root and delete it
+}
+
 function deleteNode(root: TreeNode | null, key: number): TreeNode | null {
     let cur: TreeNode | null = root
     let curParent: TreeNode | null = null
@@ -22,31 +55,7 @@ function deleteNode(root: TreeNode | null, key: number): TreeNode | null {
         return root
     }
 
-    if (!cur.left && !cur.right) {
-        cur = null
-    } else if (!cur.left) {
-        cur = cur.right
-    } else if (!cur.right) {
-        cur = cur.left
-    } else {
-        let successor = cur.right
-        let sucParent = cur
-
-        while (successor.left) {
-            sucParent = successor
-            successor = successor.left // find the minimum node in the right subtree
-        }
-
-        if (sucParent.val === cur.val) {
-            sucParent.right = successor.right
-        } else {
-            sucParent.left = successor.right
-        }
-
-        successor.left = cur.left
-        successor.right = cur.right
-        cur = successor // replace the root and delete it
-    }
+    cur = updateCurHelper(cur)
 
     if (!curParent) {
         return cur

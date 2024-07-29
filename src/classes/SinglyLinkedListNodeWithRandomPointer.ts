b17ec7c874cfc4a61ba9c6dd4c5649d7d4493ec8
dandelion-huang <dandelion820308@gmail.com>
2024-07-29 17:18:42 +0800

    refactor: 💡 remove unnecessary declare keyword and properties

--- src/classes/SinglyLinkedListNodeWithRandomPointer.ts
+++ src/classes/SinglyLinkedListNodeWithRandomPointer.ts
@@ -1,8 +1,6 @@
 import { ListNode as SinglyLinkedListNode } from 'classes/SinglyLinkedListNode'
 
 class ListNode extends SinglyLinkedListNode {
-    declare val: number
-    declare next: ListNode | null
     random: ListNode | null
 
     constructor(val?: number, next?: ListNode | null, random?: ListNode | null) {
